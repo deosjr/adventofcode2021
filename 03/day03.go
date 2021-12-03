5c5d28fc798b649be1651353791cd16c50b29a81
Sjoerd Dost <deosjr@gmail.com>
2021-12-03 11:17:53 +0100

    nice bit shift magic

--- 03/day03.go
+++ 03/day03.go
@@ -1,86 +1,72 @@
 package main
 
 import (
-    "math"
+    "strconv"
 
     "github.com/deosjr/adventofcode2021/lib"
 )
 
-func getCommon(list [][12]int64) [12]int64 {
-    sum := [12]int64{}
-    for _, line := range list {
-        for i:=0;i<12;i++ {
-            sum[i] += line[i]
+var bitlength = 12 // set to 5 when using lib.Test()
+
+func getCommon(in []uint64) uint64 {
+    var common uint64
+    threshold := float64(len(in))/2.0
+    for i:=0; i<bitlength; i++ {
+        sum := 0
+        for _, n := range in {
+            if (n>>i)&1 == 1 {
+                sum++
+            }
+        }
+        if float64(sum) >= threshold {
+            common += 1<<i
         }
     }
-
-    for i:=0;i<12;i++{
-        if float64(sum[i]) >= float64(len(list))/2.0 {
-            sum[i] = 1
-        } else {
-            sum[i] = 0
-        }
-    }
-
-    return sum
+    return common
 }
 
-func inv(in [12]int64) [12]int64 {
-    out := [12]int64{}
-    for i:=0; i<12; i++ {
-        if in[i] == 0 {
-            out[i] = 1
+func inv(in uint64) uint64 {
+    var out uint64
+    for i:=0; i<bitlength; i++ {
+        if (in>>i)&1 == 0 {
+            out += 1<<i
         }
     }
     return out
 }
 
-func toInt(in [12]int64) int64 {
-    var out int64
-    for i:=0; i<12; i++ {
-        if in[11-i] == 0 {
-            continue
-        }
-        out += int64(math.Exp2(float64(i)))
-    }
-    return out
-}
-
-func part2(input [][12]int64, inverse bool) int64 {
+func part2(input []uint64, inverse bool) uint64 {
     prev := input
-    next := [][12]int64{}
-    for i:=0; i<12; i++ {
+    next := []uint64{}
+    for i:=bitlength-1; i>=0; i-- {
         common := getCommon(prev)
         if inverse {
             common = inv(common)
         }
-        var bit int64 = common[i]
-        for _, line := range prev {
-            if line[i] == bit {
-                next = append(next, line)
+        var bit uint64 = (common>>i)&1
+        for _, n := range prev {
+            if (n>>i)&1 == bit {
+                next = append(next, n)
             }
         }
         if len(next) == 1 {
-            return toInt(next[0])
+            return next[0]
         }
         prev = next
-        next = [][12]int64{}
+        next = []uint64{}
     }
     panic("incorrect")
 }
 
 func main() {
-    input := [][12]int64{}
+    input := []uint64{}
     lib.ReadFileByLine(3, func(s string) {
-        line := [12]int64{}
-        for i, v := range s {
-            line[i] = lib.MustParseInt(string(v))
-        }
-        input = append(input, line)
+        n, _ := strconv.ParseUint(s, 2, 64)
+        input = append(input, n)
     })
 
-    sum := getCommon(input)
-    lib.WritePart1("%d", toInt(sum) * toInt(inv(sum)))
+    common := getCommon(input)
+    lib.WritePart1("%d", common * inv(common))
 
     lib.WritePart2("%d", part2(input, false) * part2(input, true))
 }
--- lib/io.go
+++ lib/io.go
@@ -8,10 +8,23 @@ import (
     "strconv"
 )
 
+// if set to true readfile will look for filename "test" instead of dayX.input
+var test bool
+
+func Test() {
+    test = true
+}
+
+func fileName(day int) string {
+    if test {
+        return fmt.Sprintf("%02d/test", day)
+    }
+    return fmt.Sprintf("%02d/day%02d.input", day, day)
+}
+
 // ReadFile returns the entire file as one big string
 func ReadFile(day int) string {
-    filename := fmt.Sprintf("%02d/day%02d.input", day, day)
-    bytes, err := ioutil.ReadFile(filename)
+    bytes, err := ioutil.ReadFile(fileName(day))
     if err != nil {
         panic(err)
     }
@@ -21,8 +34,7 @@ func ReadFile(day int) string {
 // ReadFileByLine takes a function fn, which updates a data structure
 // for each line in the input file. fn has to typecheck datastruct
 func ReadFileByLine(day int, fn func(string)) {
-    filename := fmt.Sprintf("%02d/day%02d.input", day, day)
-    f, err := os.Open(filename)
+    f, err := os.Open(fileName(day))
     if err != nil {
         panic(err)
     }
