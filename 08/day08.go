52cdf6a168aaebb68ac04b78947962cd7054ba10
Sjoerd Dost <deosjr@gmail.com>
2021-12-08 16:40:25 +0100

    optimise go/prolog, use clpfd

--- 08/day08.go
+++ 08/day08.go
@@ -33,78 +33,94 @@ func main() {
 
     lib.WritePart1("%d", sum)
 
-    sum = 0
-    for _, testline := range input {
-
     // for part2 we escape to prolog
-    prologline := "line([["
-    signals := make([]string, len(testline.signals))
-    for i, s := range testline.signals {
-        signals[i] = strings.Join(strings.Split(strings.ToUpper(s), ""), ",")
+    prologlines := []string{}
+    for _, testline := range input {
+        prologline := "line([A,B,C,D,E,F,G], [["
+        signals := make([]string, len(testline.signals))
+        for i, s := range testline.signals {
+            signals[i] = strings.Join(strings.Split(strings.ToUpper(s), ""), ",")
+        }
+        prologline += strings.Join(signals, "],[")
+        prologline += "]],[["
+        outputs := make([]string, len(testline.output))
+        for i, s := range testline.output {
+            outputs[i] = strings.Join(strings.Split(strings.ToUpper(s), ""), ",")
+        }
+        prologline += strings.Join(outputs, "],[")
+        prologline += "]])."
+        prologlines = append(prologlines, prologline)
     }
-    prologline += strings.Join(signals, "],[")
-    prologline += "]],[["
-    outputs := make([]string, len(testline.output))
-    for i, s := range testline.output {
-        outputs[i] = strings.Join(strings.Split(strings.ToUpper(s), ""), ",")
-    }
-    prologline += strings.Join(outputs, "],[")
-    prologline += "]])."
 
     pl := fmt.Sprintf(`%%%%%%
 :- use_module(library(clpfd)).
 
 run :-
-    line(Input, Output),
+    findall(Vars-Input-Output, line(Vars, Input, Output), Lines),
+    maplist(match_wires, Lines, Wires),
+    sum(Wires, #=, Ans),
+    format("~w", [Ans]).
+
+match_wires(Vars-Input-Output, Ans) :-
+    length(Vars, 7),
+    Vars ins 0..6,
+    all_distinct(Vars),
+    line(Vars, Input, Output),
     partition([X]>>(length(X,2)), Input, [One], Rest1),
     match_digit(1, One),
     partition([X]>>(length(X,3)), Rest1, [Seven], Rest7),
     match_digit(7, Seven),
     partition([X]>>(length(X,4)), Rest7, [Four], Rest4),
     match_digit(4, Four),
-    %%%% 9 should give us 'g', only signal of len 6 with only one unbound variable now
     include([X]>>length(X,6), Rest4, Len6s),
-    partition(numvars(1), Len6s, [Nine], _Rest9),
-    match_digit(9, Nine),
-    %%%% similarly 2 should now give us 'e'
-    include([X]>>length(X,5), Rest4, Len5s),
-    partition(numvars(1), Len5s, [Two], _Rest2),
-    match_digit(2, Two),
-    %% sanity check 8 but we should have everyting now
-    partition([X]>>(length(X,7)), Rest4, [Eight], _Rest8),
-    match_digit(8, Eight),
+    %% each number in the len6 list allows us to collapse one option:
+    %% 0 gives us b(1) vs d(3), 6 gives f(5) vs c(2) and 9 gives g(6) vs e(4)
+    maplist(unique_domain, Len6s),
+    %% output
     maplist(sort, Output, Sorted),
     maplist([X,Y]>>digit(Y,X), Sorted, Digits),
-    list2int(Digits, Ans),
-    format("~w", [Ans]).
+    list2int(Digits, Ans).
 
 match_digit(N, Vars) :-
-    permutation(Vars, Perm),
-    digit(N, Perm).
+    digit(N, List),
+    list2domain(List, Domain),
+    Vars ins Domain.
 
-numvars(0, []).
-numvars(N, [H|T]) :-
-    numvars(X, T),
-    ( var(H) -> N#=X+1 ; N#=X ).
+unique_domain(Vars) :-
+    exclude([X]>>fd_size(X,1), Vars, AmbiguousVars),
+    maplist([X,Y]>>fd_dom(X,Y), AmbiguousVars, Domains),
+    include([X]>>(fd_dom(X,D), unique(D,Domains)), AmbiguousVars, [Unique]),
+    fd_dom(Unique, Dom),
+    lookup(Dom, Digit),
+    Unique #= Digit.
+
+unique(Elem, List) :-
+    include([X]>>(X=Elem), List, [_]).
+
+lookup(1\/3, 1).
+lookup(2\/5, 5).
+lookup(4\/6, 6).
 
 list2int(List, N) :-
-    list2int(List, 0, N).
-list2int([], X, X).
-list2int([H|T], X, Y) :-
-    N #= X*10+H,
-    list2int(T, N, Y).
+    foldl([X,Y,Z]>>(Z#=Y*10+X), List, 0, N).
 
-digit(0, [a,b,c,e,f,g]).
-digit(1, [c,f]).
-digit(2, [a,c,d,e,g]).
-digit(3, [a,c,d,f,g]).
-digit(4, [b,c,d,f]).
-digit(5, [a,b,d,f,g]).
-digit(6, [a,b,d,e,f,g]).
-digit(7, [a,c,f]).
-digit(8, [a,b,c,d,e,f,g]).
-digit(9, [a,b,c,d,f,g]).
-%s`, prologline)
+list2domain([H], H).
+list2domain([H|T], Dom) :-
+    list2domain(T, D),
+    Dom = D\/H.
+
+digit(0, [0,1,2,4,5,6]).
+digit(1, [2,5]).
+digit(2, [0,2,3,4,6]).
+digit(3, [0,2,3,5,6]).
+digit(4, [1,2,3,5]).
+digit(5, [0,1,3,5,6]).
+digit(6, [0,1,3,4,5,6]).
+digit(7, [0,2,5]).
+digit(8, [0,1,2,3,4,5,6]).
+digit(9, [0,1,2,3,5,6]).
+
+%s`, strings.Join(prologlines, "\n"))
 
     err := os.WriteFile("temp.pl", []byte(pl), 0644)
     if err != nil {
@@ -122,8 +138,5 @@ digit(9, [a,b,c,d,f,g]).
     }
 
     out := lib.MustParseInt(string(rawout))
-    sum += out
-    }
-
-    lib.WritePart2("%d", sum)
+    lib.WritePart2("%d", out)
 }
