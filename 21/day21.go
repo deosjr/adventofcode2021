f3b8447534c31f73f5c457c3eba8392ec927241a
Sjoerd Dost <deosjr@gmail.com>
2021-12-21 12:28:21 +0100

    day21 optimised

--- 21/day21.go
+++ 21/day21.go
@@ -6,21 +6,95 @@ import (
     "github.com/deosjr/adventofcode2021/lib"
 )
 
-func modPlus1(n, m int) int {
-    x := n % m
-    if x == 0 {
-        x = m
+var mem = map[int]int{}
+
+func mod10Plus1(n int) int {
+    if v, ok := mem[n]; ok {
+        return v
     }
+    x := ((n-1) % 10) + 1
+    mem[n] = x
     return x
 }
 
 type universe struct {
-    p1pos int
-    p1score int
-    p2pos int
-    p2score int
+    activepos int
+    activescore int
+    passivepos int
+    passivescore int
     p2turn bool // if false, its p1's turn
+}
+
+func part1(u universe) int {
+    var die int
+    dierolls := 0
+    for u.passivescore < 1000 {
+        sum := 0
+        for i:=0; i<3; i++ {
+            die = (die % 100) + 1
+            dierolls++
+            sum += die
+        }
+        newpos := mod10Plus1(u.activepos + sum)
+        u.activepos, u.passivepos = u.passivepos, newpos
+        u.activescore, u.passivescore = u.passivescore, u.activescore + u.passivepos
+        u.p2turn = !u.p2turn
+    }
+    return dierolls * u.activescore
+}
+
+var diedistr = []struct{
+    sum int
     num int64
+}{
+    {sum:3, num:1},
+    {sum:4, num:3},
+    {sum:5, num:6},
+    {sum:6, num:7},
+    {sum:7, num:6},
+    {sum:8, num:3},
+    {sum:9, num:1},
+}
+
+type in struct {
+    u universe
+    n int64
+}
+
+type out struct {
+    p1 int64
+    p2 int64
+}
+
+var cache = map[in]out{}
+
+func part2(u universe, n int64) (int64, int64) {
+    if u.passivescore >= 21 {
+        if u.p2turn {
+            return n, 0
+        } else {
+            return 0, n
+        }
+    }
+    v, ok := cache[in{u, n}]
+    if ok {
+        return v.p1, v.p2
+    }
+    var p1sum, p2sum int64
+    newu := universe{
+        activepos: u.passivepos,
+        activescore: u.passivescore,
+    }
+    for _, d := range diedistr {
+        newu.passivepos = mod10Plus1(u.activepos + d.sum)
+        newu.passivescore = u.activescore + newu.passivepos
+        newu.p2turn = !u.p2turn
+        p1, p2 := part2(newu, d.num)
+        p1sum += p1
+        p2sum += p2
+    }
+    cache[in{u, n}] = out{n*p1sum, n*p2sum}
+    return n*p1sum, n*p2sum
 }
 
 func main() {
@@ -29,86 +103,10 @@ func main() {
     fmt.Sscanf(input[0], "Player 1 starting position: %d", &p1)
     fmt.Sscanf(input[1], "Player 2 starting position: %d", &p2)
 
-    state := universe{p1pos: p1, p2pos: p2}
-    var die int
-    dierolls := 0
-    for state.p1score < 1000 && state.p2score < 1000 {
-        die = modPlus1(die+1, 100)
-        dierolls++
-        sum := die
-        die = modPlus1(die+1, 100)
-        dierolls++
-        sum += die
-        die = modPlus1(die+1, 100)
-        dierolls++
-        sum += die
-        if state.p2turn {
-            state.p2pos = modPlus1(state.p2pos + sum, 10)
-            state.p2score += state.p2pos
-            state.p2turn = false
-            continue
-        }
-        state.p1pos = modPlus1(state.p1pos + sum, 10)
-        state.p1score += state.p1pos
-        state.p2turn = true
-    }
-    ans1 := dierolls*state.p1score
-    if state.p2score < state.p1score {
-        ans1 = dierolls*state.p2score
-    }
+    ans1 := part1(universe{activepos: p1, passivepos: p2})
     lib.WritePart1("%d", ans1)
 
-    universes := []universe{{p1pos: p1, p2pos: p2, num: 1}}
-    diedistr := []struct{
-        sum int
-        num int64
-    }{
-        {sum:3, num:1},
-        {sum:4, num:3},
-        {sum:5, num:6},
-        {sum:6, num:7},
-        {sum:7, num:6},
-        {sum:8, num:3},
-        {sum:9, num:1},
-    }
-    var p1sum, p2sum int64
-    for len(universes) > 0 {
-        u := universes[0]
-        universes = universes[1:]
-        if u.p1score >= 21 {
-            p1sum += u.num
-            continue
-        }
-        if u.p2score >= 21 {
-            p2sum += u.num
-            continue
-        }
-        for _, dist := range diedistr {
-            if u.p2turn {
-                newpos := modPlus1(u.p2pos+dist.sum, 10)
-                newu := universe{
-                    p1pos:   u.p1pos,
-                    p1score: u.p1score,
-                    p2pos:   newpos,
-                    p2score: u.p2score + newpos,
-                    p2turn:  false,
-                    num:     u.num * dist.num,
-                }
-                universes = append(universes, newu)
-                continue
-            }
-            newpos := modPlus1(u.p1pos+dist.sum, 10)
-            newu := universe{
-                p1pos:   newpos,
-                p1score: u.p1score + newpos,
-                p2pos:   u.p2pos,
-                p2score: u.p2score,
-                p2turn:  true,
-                num:     u.num * dist.num,
-            }
-            universes = append(universes, newu)
-        }
-    }
+    p1sum, p2sum := part2(universe{activepos: p1, passivepos: p2}, 1)
     ans2 := p1sum
     if p2sum > p1sum {
         ans2 = p2sum
